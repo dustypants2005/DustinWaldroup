760d4be36ebf7a7cee3619431134ab14467cb168
dustinWaldroup <dustypants2005@hotmail.com>
2015-12-31 15:46:25 -0500

    configuring and removing some grunt left overs

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,13 @@
 
 'use strict';
 
-var gulp = require('gulp');
-var wrench = require('wrench');
+var gulp = require('gulp'),
+    gutil = require('gutil'),
+    wrench = require('wrench'),
+    connect = require('gulp-connect'),
+    watch = require('gulp-watch'),
+    less = require('gulp-less'),
+    coffee = require('gulp-coffee');
 
 /**
  *  This will load all js or coffee files in the gulp directory
@@ -27,3 +32,39 @@ wrench.readdirSyncRecursive('./gulp').filter(function(file) {
 gulp.task('default', ['clean'], function () {
   gulp.start('build');
 });
+
+gulp.task('default', function() {
+  return gutil.log('Gulp is running!')
+});
+
+gulp.task('webserver', function() {
+  connect.server({
+    livereload: true,
+    root: ['.', '.tmp']
+  });
+});
+
+gulp.task('livereload', function() {
+  gulp.src(['.tmp/styles/*.css', '.tmp/scripts/*.js'])
+    .pipe(watch())
+    .pipe(connect.reload());
+});
+
+gulp.task('less', function() {
+  gulp.src('styles/main.less')
+    .pipe(less())
+    .pipe(gulp.dest('.tmp/styles'));
+});
+
+gulp.task('coffee', function() {
+  gulp.src('scripts/*.coffee')
+    .pipe(coffee())
+    .pipe(gulp.dest('.tmp/scripts'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('styles/*.less', ['less']);
+  gulp.watch('scripts/*.coffee', ['coffee']);
+})
+
+gulp.task('default', ['less', 'coffee', 'webserver', 'livereload', 'watch']);
